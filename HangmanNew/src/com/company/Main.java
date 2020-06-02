4cf1dbc777f6516c12445b2b9e146f3c157d1036
Dominik Hoffmann <47928011+dh1542@users.noreply.github.com>
2020-06-02 21:51:00 +0200

    New version. Playable in the console now.
    The file path is not accurate.

--- HangmanNew/src/com/company/Main.java
+++ HangmanNew/src/com/company/Main.java
@@ -1,6 +1,9 @@
 package com.company;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -18,28 +21,75 @@ public class Main {
 
 
         String word = WordReader.getRandomNoun(wordList);
-        System.out.println(word);
+
 
         if(word != null){
             char[] guess = new char[word.length()];
             int chances = 6;
             int count = 0;
+            List<Character> banned = new ArrayList<Character>();
+
+            int charCounter = 0;
+
+
+
 
             while(count != chances){
                 Scanner reader = new Scanner(System.in);
+                System.out.println("Your guess (first character will count): ");
                 char c = reader.next().charAt(0);
-                System.out.println(Character.isAlphabetic(c));
 
 
-                if(word.indexOf(c) == -1){
-                    count++;
+
+
+                if(Character.isAlphabetic(c) && !banned.contains(c)){
+                    if(word.indexOf(c) == -1){
+                        banned.add(c);
+
+                        count++;
+                    }
+
+
+                    else{
+                        List<Integer> temp = new ArrayList<Integer>();
+                        for(int i = 0; i < word.length(); i++){
+                            if(word.charAt(i) == c){
+                                guess[i] = c;
+                                charCounter++;
+                            }
+                        }
+
+
+
+
+                    }
+
+                    for(int j = 0; j < guess.length; j++){
+                        System.out.println(j + ": " + guess[j]);
+                    }
+                    String guessS = Arrays.toString(guess);
+                    if(guessS.contains(",") || guessS.contains("]") || guessS.contains("[")){
+                        guessS = guessS.replace(",", "");
+                        guessS = guessS.replace("[", "");
+                        guessS = guessS.replace("]", "");
+                        guessS = guessS.replace(" ", "");
+                    }
+
+                    if(charCounter == word.length()){
+                        System.out.println("You won!");
+                        break;
+                    }
+
+
+
+
+                    System.out.println(guessS);
+
+
+                    System.out.println("You have " + (chances - count) + " trys left.");
+
+
                 }
-                else{
-
-
-
-
-                }
 
 
 
@@ -48,6 +98,22 @@ public class Main {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+            if(chances == count){
+                System.out.println("The word was: " + word);
+                System.out.println("You lost!");
             }
 
 
